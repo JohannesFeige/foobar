d99690536cdae02406796ef01f205907074f4728
Tim Dalkiewicz <tim.dalkiewicz@aclue.de>
2023-07-27 19:02:12 +0200

    aenderungen fue uebung 1

--- src/main/java/de/aclue/graphqltraining/superheroes/api/graphql/SuperheroController.java
+++ src/main/java/de/aclue/graphqltraining/superheroes/api/graphql/SuperheroController.java
@@ -1,9 +1,16 @@
 package de.aclue.graphqltraining.superheroes.api.graphql;
 
+import de.aclue.graphqltraining.superheroes.entities.Superhero;
 import de.aclue.graphqltraining.superheroes.services.SuperheroService;
 import lombok.RequiredArgsConstructor;
 
+import org.springframework.data.domain.Page;
+import org.springframework.graphql.data.method.annotation.Argument;
+import org.springframework.graphql.data.method.annotation.QueryMapping;
+import org.springframework.graphql.data.method.annotation.SchemaMapping;
 import org.springframework.stereotype.Controller;
+
+import java.util.List;
 
 @Controller
 @RequiredArgsConstructor
@@ -11,6 +18,14 @@ public class SuperheroController {
 
     private final SuperheroService superheroService;
 
-    //TODO Aufgabe 1
+    @SchemaMapping(typeName = "Query", value = "allSuperheros")
+    public List<Superhero> allSuperherosOtherMethodName(){
+        return superheroService.findAll();
+    }
+
+    @QueryMapping
+    public Page<Superhero> allSuperherosPaged(@Argument int page, @Argument int size){
+        return superheroService.findAll(page, size);
+    }
 
 }
